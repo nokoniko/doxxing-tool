3aa6f6e7345ed483cf832da4b5614a33370eed3b
Niko <94224292+nokoniko@users.noreply.github.com>
2025-05-27 22:16:17 +0200

    Update phone.py

--- tools/phone.py
+++ tools/phone.py
@@ -5,4 +5,4 @@ def phone():
     phone = input(f"{Fore.GREEN}[?]{Fore.BLUE} input phone number:{Fore.RESET} ").lower()
     webbrowser.open(f"https://www.whitepages.com/phone/{phone}")
     time.sleep(5)
-    webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ") # yes this is so funny
