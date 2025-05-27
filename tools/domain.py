7f7430007e42f1f66f461069b3109b696b248401
Niko <94224292+nokoniko@users.noreply.github.com>
2025-05-27 22:15:51 +0200

    Update domain.py

--- tools/domain.py
+++ tools/domain.py
@@ -5,5 +5,5 @@ def domain_look():
     domain = input(f"{Fore.GREEN}[?]{Fore.BLUE} input domain:{Fore.RESET} ").lower()
     webbrowser.open(f"https://www.norid.no/?s={domain}")
     time.sleep(5)
-    webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ") # yes this is so funny
     
