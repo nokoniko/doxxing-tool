1b187f755db23246d14df72474c6b78a9996af22
Niko <94224292+nokoniko@users.noreply.github.com>
2025-05-27 22:16:09 +0200

    Update name.py

--- tools/name.py
+++ tools/name.py
@@ -7,5 +7,5 @@ def name_look():
     why = input(f"{Fore.GREEN}[?]{Fore.BLUE} input name:{Fore.RESET} ").lower()
     webbrowser.open(f"https://www.whitepages.com/name/{hostname}?fs=1&q={hostname}")
     time.sleep(5)
-    webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ") # yes this is so funny
     
